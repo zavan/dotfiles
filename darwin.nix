2826a684b662b022bf44dcbefa975a9f2b934107
Felipe Zavan <felipe@zavan.me>
2024-06-10 13:17:24 +0100

    Classic...

--- darwin.nix
+++ darwin.nix
@@ -10,7 +10,7 @@
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  environment.darwinConfig = "$HOME/projects/dotfiles"
+  environment.darwinConfig = "$HOME/projects/dotfiles";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
